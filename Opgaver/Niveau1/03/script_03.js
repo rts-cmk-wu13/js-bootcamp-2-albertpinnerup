d2f8700d471337172de45d0241865837842193bf
Albert Pinnerup <albert.pinnerup@gmail.com>
2025-01-30 12:30:02 +0100

    blabla

--- Opgaver/Niveau1/03/script_03.js
+++ Opgaver/Niveau1/03/script_03.js
@@ -1,9 +1,15 @@
-function skjulElementViaCssSelector(cssSelector) {
-    let element = document.querySelector(cssSelector);
+function skjulElementerViaCssSelector(cssSelector) {
+    let elements = document.querySelectorAll(cssSelector);
 
-    if (element !== null) {
-        element.style.display = "none"
-    }
+    elements.forEach(function(element) {
+        skjulElement(element)
+    })
+    
 }
 
+
+function skjulElement(element) {
+    if(element !== null) element.style.display = "none"
+}
 skjulElementViaCssSelector("#my-button");
+
