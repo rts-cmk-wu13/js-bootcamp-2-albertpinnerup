6a7fb743096c3135518faf6ec62b27e9b55ca7a2
Albert Pinnerup <albert.pinnerup@gmail.com>
2025-01-30 11:25:08 +0100

    finished extra task in niv1 opg2

--- Opgaver/Niveau1/02/script.js
+++ Opgaver/Niveau1/02/script.js
@@ -2,6 +2,19 @@ function beregnForhold(a, b) {
     return a / b
 }
 
-resultat = beregnForhold(4, 3) 
+
+
+function beregnForhold2(a, b) {
+    if (b == 0) return null
+    
+
+    return a / b
+}
+
+let resultat = beregnForhold(4, 3)
+
+let resultat2 = beregnForhold2(4, 0)
+
+console.log(resultat2);
 
 console.log(resultat);
