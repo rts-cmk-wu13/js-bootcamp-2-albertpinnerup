34dc256c4a9a7aaf31340757fef375a5da20ef25
Albert Pinnerup <albert.pinnerup@gmail.com>
2025-01-30 11:16:47 +0100

    finished niv2 opg2

---
+++ Opgaver/Niveau2/02/script.js
@@ -0,0 +1,38 @@
+const addButtons = document.querySelectorAll(".product__add");
+const subtractButtons = document.querySelectorAll(".product__subtract");
+const amounts = document.querySelectorAll(".product__amount");
+const prices = document.querySelectorAll(".product__price");
+
+const originalPrices = [15, 20, 30];
+
+
+
+
+addButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        
+        let currentAmount = parseInt(amounts[index].textContent);
+        currentAmount++
+
+        amounts[index].textContent = currentAmount;
+
+        prices[index].textContent = originalPrices[index] * currentAmount;
+        
+        console.log(index);
+        
+    });
+});
+
+subtractButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        let currentAmount = parseInt(amounts[index].textContent)
+
+        if (currentAmount > 0) {
+
+            currentAmount--
+            amounts[index].textContent = currentAmount
+
+            prices[index].textContent = originalPrices[index] * currentAmount;
+        } 
+    });
+})
